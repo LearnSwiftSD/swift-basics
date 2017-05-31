5c1caf76affeb8b6f690bf1eae179840a5256b32
Guillermo Alvarez <galvarezcity@me.com>
2017-05-31 08:34:54 -0700

    code after presentation

--- 1-Basics.playground/Contents.swift
+++ 1-Basics.playground/Contents.swift
@@ -3,23 +3,107 @@
 //: - Variables and constants
 
 //:	let vs var
-
+let someConstant = 10
+var someVariable = 50
+someVariable = 100
 //: - Type inference
+let apple = "🍎"
+let orange = "🍊"
+
+//:
+let wifiPassword = "4875esubwireless"
+var something = 20
+
 
 //:		You don't always need to specify type
 
 //: - Printing
 
 //:		print or dump
+//print(orange)
+//print(apple)
 
+struct Rectangle {
+	let width: Double
+	let height: Double
+}
+
+let rectangle = Rectangle(width: 20, height: 40)
+//print(rectangle)
+//dump(rectangle)
 //: - Tuples
 
 //:			Tuples are great
-let (x,y) = (12.0, "Happy Camp")
-print(x)
+let (x,y) = ("Some string", 20)
+//print((x,y))
+//
+//dump((x,y))
+//
+//print(x)
+//print(y)
+//print((x,y).0)
 //: - Optionals
 
 //:		An optional might be nil
+let mightBeNil:String?
+
+mightBeNil = "hello"
+
+let anotherOptional: String?
+
+anotherOptional = "world"
+
+// THIS IS VERY VERY BAD!!!!!!!!!!!!!!!!!
+let helloWorld = mightBeNil! + " " + anotherOptional!
+//print(helloWorld)
+
+//Some good options:
+
+// Translation: If mightBeNil is not nil, then the variable unwrappedValue holds the value of mightBeNil but it is no longer an optional
+// This is pretty good
+//print(mightBeNil)
+if
+	let unwrappedValue = mightBeNil,
+	let anotherUnwrappedValue = anotherOptional {
+
+//		print(unwrappedValue + anotherUnwrappedValue)
+}
+
+// You can also use guard let
+
+guard
+	let unwrappedValue = mightBeNil,
+	let anotherUnwrappedValue = anotherOptional else {
+		fatalError("OH NO!!!")
+}
+
+print(unwrappedValue + " " + anotherUnwrappedValue)
+//Now I can use the unwrapped values
+
+
+// ALSO USUALLY BAD!
+//Implicitly unwrapped Optionals
+
+let thisIsDangerous: String!
+
+//print(thisIsDangerous)
+
+// Nil coalescing operator
+
+let w: String? = nil
+
+let defaultValue: String = w ?? "default"
+
+
+//if w != nil {
+//	defaultValue = w!
+//} else {
+//	defaultValue = "default"
+//}
+
+print(defaultValue)
+
+
 
 //:		How to work with them
 
@@ -30,6 +114,18 @@ print(x)
 //:
 
 //:		Optional chaining
+struct Room {
+	let numberOfWalls: Int
+	let numberOfToys: Int?
+}
+
+struct House {
+	let room: Room?
+	let kitchen: Int
+}
+let room = Room(numberOfWalls: 5, numberOfToys: 10)
+let house = House(room: room, kitchen: 2)
+print(house.room?.numberOfWalls)
 
 
 //: ## 2. Operators
@@ -75,7 +171,7 @@ print(x)
 //: ## 9. Classes and Structures... introduction to Protocols
 
 //:	- Diffrences between classes and structs
-
+go
 //:		Value vs Reference Types
 
 //: ## 10. Properties
